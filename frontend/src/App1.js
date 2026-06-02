4959184229509ae298929f051d7ef8646b5e8afe
Akhil Siva Chowdary Nandigam <akhilsivachowdary11@gmail.com>
2026-06-02 14:21:32 +0530

    Delete frontend/src/App1.js

--- frontend/src/App1.js
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>🚧 Site Under Maintenance</h1>
-      <p>Frontend is temporarily unavailable.</p>
-    </div>
-  );
-}
